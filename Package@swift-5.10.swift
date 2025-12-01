78ca1c240b2ec4413574ae464db84363f537c019
Sergey Romanenko <70233726+purpln@users.noreply.github.com>
2025-12-01 22:41:33 +0300

    no message

--- Package@swift-5.10.swift
+++ Package@swift-5.10.swift
@@ -5,8 +5,7 @@ import PackageDescription
 let package = Package(name: "TinyFoundation", products: [
     .library(name: "TinyFoundation", targets: ["TinyFoundation"]),
 ], dependencies: [
-    .package(path: "../libc"),
-    //.package(url: "https://github.com/purpln/libc.git", branch: "main"),
+    .package(url: "https://github.com/purpln/libc.git", branch: "main"),
 ], targets: [
     .target(name: "TinyFoundation", dependencies: [
         .product(name: "LibC", package: "libc"),
